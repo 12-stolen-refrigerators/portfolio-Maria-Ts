0d334c6dcbf4ffe2316b5333463d961a9d956bdb
Maria Tsval <130552049+12-stolen-refrigerators@users.noreply.github.com>
2023-05-30 17:45:32 +0300

    Add files via upload

---
+++ router.php
@@ -0,0 +1,21 @@
+<?php
+
+$url = explode("/", $_SERVER['REQUEST_URI']);
+
+if ($url[1] == "main") {
+    $content = file_get_contents("pages/index.html");
+} else if ($url[1] == "portfolio") {
+    $content = file_get_contents("pages/portfolio-places.html");
+} else if ($url[1] == "portfolio-places") {
+    $content = file_get_contents("pages/portfolio-places.html");
+} else if ($url[1] == "about-me") {
+    $content = file_get_contents("pages/about-me.html");
+} else if ($url[1] == "contacts") {
+    $content = file_get_contents("pages/contacts.html");
+} else {
+    $content = file_get_contents("pages/index.html");
+}
+
+if (!empty($content)) {
+    require_once("template.php");
+}
---
+++ template.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="ru">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe:ital@0;1&family=Alumni+Sans:ital,wght@0,100;0,300;0,400;1,100&family=Cormorant:ital,wght@0,300;0,400;0,600;1,300;1,400&family=Oswald:wght@400;600;700&family=Raleway:wght@400;600&display=swap" rel="stylesheet">
+		<link rel="shortcut icon" href="/img/refr-favicon-32x32.png">
+		<link rel="stylesheet" href="/css/style.css">
+		<script src="https://kit.fontawesome.com/db4f42a312.js" crossorigin="anonymous"></script>
+		<title>Maria Ts' 13-th refrigerator</title>
+
+	</head>
+	<body>
+		
+		<div class="container">
+		<div class="row">
+			
+			<img src="/img/refrig2.png" alt="" style="height: 60px;" class="mx-auto">
+			
+		</div>
+		<nav class="nav nav-pills flex-column flex-sm-row">
+		
+		<a class="flex-sm-fill text-sm-center nav-link active-tab" href="/main">Главная</a>
+
+			<a class="flex-sm-fill text-sm-center nav-link dropdown-toggle" href="/portfolio" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			Портфолио
+			</a>
+		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			<a class="dropdown-item" href="/portfolio-places">Места</a>
+			<a class="dropdown-item" href="/portfolio-things">Объекты</a>
+			<a class="dropdown-item" href="/portfolio-people">Люди</a>
+		</div>
+
+		<a class="flex-sm-fill text-sm-center nav-link" href="/about-me">Обо мне</a>
+		<a class="flex-sm-fill text-sm-center nav-link" href="/contacts">Контакты</a>
+		</nav>
+		</div>
+		
+
+		<?php echo $content; ?>
+
+		<!-- Вариант 1: пакет jQuery и Bootstrap (включает Popper) -->
+		<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+		
+	</body>
+</html>
